87a2d5f6d4d755a17e6af7696712381d191e196e
Thuringud <rom1234593@yandex.ru>
2022-07-11 16:01:43 +0300

    vue-crud

---
+++ src/components/UserCreate.vue
@@ -0,0 +1,42 @@
+<template>
+	<div class="card card-body mt-4">
+		<form @submit.prevent="onSubmit">
+			<div class="form-group">
+				<label>Имя</label>
+				<input v-model="form.name" class="form-control" required>
+			</div>
+
+			<div class="form-group mt-3">
+				<label>Почта</label>
+				<input v-model="form.email" class="form-control" type="email" required>
+			</div>
+
+			<button type="submit" class="btn btn-success mt-3">
+				Создать пользователя
+			</button>
+		</form>
+	</div>
+</template>
+
+<script>
+import {createUser} from '@/firebase'
+import {reactive} from 'vue'
+
+export default {
+	setup() {
+		const form = reactive({name: '', email: ''})
+
+		const onSubmit = async () => {
+			await createUser({...form})
+			form.name = ''
+			form.email = ''
+		}
+
+		return {form, onSubmit}
+	}
+}
+</script>
+
+<style scoped>
+
+</style>
